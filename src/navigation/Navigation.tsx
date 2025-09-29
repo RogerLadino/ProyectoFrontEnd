592d15bea76880b9501f85a48e6ee4ae43d5ab7d
TomasSanchez119 <tsanchezavellaneda@gmail.com>
2025-09-28 19:53:08 -0500

    fix: Remueve router para resolver errores al ejecutar el frontend

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -3,10 +3,8 @@ import { ListExerciseView } from "../views/Exercise/ListExerciseView";
 
 export const Navigation = () => {
   return (
-    <Router>
       <Routes>
         <Route path="/classroom/:classroomId/exercises" element={<ListExerciseView />} />
       </Routes>
-    </Router>
   );
 }
