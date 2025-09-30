acc1b6a1cec4cdfabc12df959dbbe1ede814b6d8
Roger Ladino <ztroger15@gmail.com>
2025-09-30 17:41:08 -0500

    feat(realtime): Agrega código en tiempo real

--- src/views/Code/CodeView.jsx
+++ src/views/Code/CodeView.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   FiUser,
   FiCircle,
@@ -9,6 +9,7 @@ import {
   FiCheckCircle,
 } from "react-icons/fi";
 import AceEditor from "react-ace";
+import * as signalR from "@microsoft/signalr";
 
 // Importar lenguajes y tema de Ace
 import "ace-builds/src-noconflict/mode-python";
@@ -19,17 +20,82 @@ import "ace-builds/src-noconflict/ext-language_tools";
 
 import "../../styles/codigo.css";
 import { TopBar } from "../../components/Navigation/TopBar";
+import { Link, useParams } from "react-router-dom";
+import { getExercisesById } from "../../services/exercises.service";
+import { getSubmissionByUserId } from "../../services/submission.service";
 
 export function CodeView() {
   const [activeTab, setActiveTab] = useState("ejercicio");
   const [language, setLanguage] = useState("python");
   const [code, setCode] = useState("");
+  const { classroomId, exerciseId, userId } = useParams();
+  const [exercise, setExercise] = useState({});
+  const [submission, setSubmission] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const exerciseData = await getExercisesById(classroomId, exerciseId);
+        const submissionData = await getSubmissionByUserId(parseInt(exerciseId), parseInt(userId));
+
+        setExercise(exerciseData);
+        setSubmission(submissionData);
+      } catch (e) {
+
+      }
+    }
+
+    fetchData();
+  }, [])
 
   const isProfessor = true;
 
+  const connectionRef = useRef(null);
+
+  useEffect(() => {
+    if (exerciseId == null || userId == null) return;
+
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl(`${import.meta.env.VITE_API_URL}/hubs/code`)
+      .withAutomaticReconnect()
+      .build();
+
+    connection
+      .start()
+      .then(async () => {
+        connection.on("CodeInitialized", (initialCode) => {
+          if (initialCode?.sourceCode) setCode(initialCode.sourceCode);
+        });
+
+        connection.on("CodeUpdated", (updatedCode) => {
+          if (updatedCode?.sourceCode) setCode(updatedCode.sourceCode);
+        });
+
+        await connection.invoke("JoinExerciseGroup", parseInt(exerciseId), parseInt(userId));
+      })
+      .catch((err) => console.error("❌ Error al conectar:", err));
+
+    connectionRef.current = connection;
+
+    return () => {
+      connection.stop();
+    };
+  }, []);
+
   const handleCodeChange = (newValue) => {
     setCode(newValue);
-    console.log("Nuevo código:", newValue);
+
+    if (connectionRef.current) {
+      const codeDto = {
+        appUserId: parseInt(userId),
+        exerciseId: parseInt(exerciseId),
+        sourceCode: newValue,
+      };
+
+      connectionRef.current
+        .invoke("UpdateCode", codeDto)
+        .catch((err) => console.error("❌ Error enviando UpdateCode:", err));
+    }
   };
 
   return (
@@ -40,13 +106,13 @@ export function CodeView() {
       <div className="main-container">
         {/* SIDEBAR */}
         <nav className="sidebar">
-          <a
+          <Link
             className="nav-item"
-            href={isProfessor ? "ejercicio-profesor.html" : "ejercicio.html"}
+            to={`/classroom/${classroomId}/exercise/${exerciseId}`}
           >
             <FiCircle className="icon-accent" />
-            <span className="nav-item-header titulo-ejercicio">Ejercicio 1</span>
-          </a>
+            <span className="nav-item-header titulo-ejercicio">{exercise.name}</span>
+          </Link>
         </nav>
 
         {/* CONTENT */}
@@ -56,25 +122,32 @@ export function CodeView() {
             <div className="student-info">
               <div className="student-info-item">
                 <FiCircle className="icon-warning" />
-                <p className="nombre-usuario">Roger</p>
+                <p className="nombre-usuario">
+                {`${submission.appUser.firstName} ${submission.appUser.lastName}`}
+                </p>
               </div>
 
               {isProfessor && (
                 <div className="student-info-item">
                   <FiCircle className="student-icon" />
                   <p>Resuelto: </p>
-                  <FiX className="student-icon" />
+                  {submission.status == 0 ?
+                    <FiX className="student-icon" />
+                    : <FiCheckCircle className="student-icon" />
+                  }
 
                   <FiCircle className="student-icon" />
                   <p>Intentos: </p>
-                  <p className="attempts student-icon">0</p>
+                  <p className="attempts student-icon">
+                    0
+                  </p>
 
                   <FiCircle className="student-icon" />
                   <p>Nota: </p>
                   <input
                     type="text"
                     className="underline-input note-input"
-                    placeholder="0"
+                    placeholder={submission.grade}
                   />{" "}
                   /100
 
@@ -133,31 +206,20 @@ export function CodeView() {
               <div className="editor-section tabs">
                 <div className="editor-title exercise-title tab-container">
                   <div
-                    className={`editor-title-item tab ${
-                      activeTab === "ejercicio" ? "active" : ""
-                    }`}
+                    className={`editor-title-item tab ${activeTab === "ejercicio" ? "active" : ""
+                      }`}
                     onClick={() => setActiveTab("ejercicio")}
                   >
                     <FiCircle className="icon-accent" />
                     <p>Ejercicio</p>
                   </div>
                   <div
-                    className={`editor-title-item tab ${
-                      activeTab === "consola" ? "active" : ""
-                    }`}
+                    className={`editor-title-item tab ${activeTab === "consola" ? "active" : ""
+                      }`}
                     onClick={() => setActiveTab("consola")}
                   >
                     <FiCircle className="icon-accent" />
                     <p>Consola</p>
-                  </div>
-                  <div
-                    className={`editor-title-item tab ${
-                      activeTab === "ayuda" ? "active" : ""
-                    }`}
-                    onClick={() => setActiveTab("ayuda")}
-                  >
-                    <FiCircle className="icon-accent" />
-                    <p>Ayuda</p>
                   </div>
                 </div>
 
@@ -167,7 +229,7 @@ export function CodeView() {
                     <div className="exercise tab-item">
                       <div className="editor-body exercise-description">
                         <p className="descripcion-ejercicio">
-                          Aquí va la descripción del ejercicio.
+                          {exercise.description}
                         </p>
                         <div className="pruebas"></div>
                       </div>
@@ -199,22 +261,6 @@ export function CodeView() {
                           <FiClock className="console-icon" />
                         </div>
                       </div>
-                    </div>
-                  )}
-
-                  {activeTab === "ayuda" && (
-                    <div className="analysis tab-item">
-                      <div className="messages"></div>
-                      <form className="chat">
-                        <input
-                          className="chat-input"
-                          type="text"
-                          placeholder="Escribe un texto aquí"
-                        />
-                        <button className="chat-submit" type="submit">
-                          <FiSend />
-                        </button>
-                      </form>
                     </div>
                   )}
                 </div>
