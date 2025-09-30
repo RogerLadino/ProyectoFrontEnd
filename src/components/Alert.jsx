dcfb5654f1331f11e81a228a4e79da24c4884c21
NICOLAS2006-APRENDIZ <nicolasvanegas171@gmail.com>
2025-09-30 16:07:04 -0500

    WIP: avances en frontend antes de crear rama

--- src/components/Alert.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Alert({ alert }) {
-  return (
-    <div className={`alert alert-${alert.category} mt-2`} role="alert">
-      {alert.message}
-    </div>
-  );
-}
