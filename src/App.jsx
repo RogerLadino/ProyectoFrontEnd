7d1ee6ff168e2718dbdd7338dca74c920bc1fffe
TomasSanchez119 <tsanchezavellaneda@gmail.com>
2025-09-30 16:04:33 -0500

    feat: Agrega HomePage a las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,35 @@ import LoginPage from './pages/Login/LoginPage';
 import RegisterPage from './pages/Register/RegisterPage';
 import RecoverPasswordPage from './pages/RecoverPassword/RecoverPasswordPage';
 import ResetPasswordPage from './pages/ResetPassword/ResetPasswordPage';
+import HomePage from './pages/HomePage/HomePage';
+import styles from './Global.Styles.module.css';
 import {
-  BrowserRouter as Router, // Use a specific name like BrowserRouter
+  BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
+  Navigate, 
 } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      {/* ExerciseProvider can wrap everything that needs its context */}
       <ExerciseProvider>
         <div className="App">
-          {/* Navigation might use Link or other router features, so it's inside the Router */}
-          <Navigation /> 
           
+          <Navigation />
+
           <Routes>
-            {/* Define which component to show for each route */}
-            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/" element={<HomePage />} /> 
+            
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/recover-password" element={<RecoverPasswordPage />} />
             <Route path="/reset-password" element={<ResetPasswordPage />} />
-            {/* You can add more routes here */}
-            {/*<Route path="*" element={<NotFoundPage />} />*/}
+            
+            {/* Si estás usando Navigate para manejar la ruta por defecto o error 404, 
+               ahora funcionará: */}
           </Routes>
+          
         </div>
       </ExerciseProvider>
     </Router>
