2207da329c1fcc431ee689ccf6fc71eef36d9cf3
Roger Ladino <ztroger15@gmail.com>
2025-09-30 23:22:35 -0500

    fix: Arregla el import de TopBar en las vistas

--- src/components/Navigation/TopBar.jsx
+++ src/components/Navigation/TopBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const TopBar = () => {
+export default TopBar = () => {
   return (
     <nav className="navbar w-100 navbar-expand-md navbar-dark">
       <div className="container-fluid">
